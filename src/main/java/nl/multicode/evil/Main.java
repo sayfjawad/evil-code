23255ade2867d5f2cb60363358accc9112f7ec3e
sayf <sayf@multicode.nl>
2024-12-12 14:11:34 +0400

    removing evil!

--- src/main/java/nl/multicode/evil/Main.java
+++ src/main/java/nl/multicode/evil/Main.java
@@ -3,24 +3,13 @@ package nl.multicode.evil;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-import nl.multicode.evil.agent.SetupAgent;
-
 public class Main {
 
     public static void main(String[] args) {
 
         // Before redefinition
         var instance = new MyClass();
-        System.out.println("Before redefinition: " + instance.myInnocentMethod());
 
-        // Do some covert stuff!!
-        SetupAgent.execute();
-
-        // After redefinition
-        System.out.println("After redefinition: " + instance.myInnocentMethod());
-
-        // Also test a new instance to confirm the change affects the class definition globally
-        instance = new MyClass();
-        System.out.println("New instance after redefinition: " + instance.myInnocentMethod());
+        System.out.println(instance.myInnocentMethod());
     }
 }
